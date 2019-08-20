bf3f9fe286bd574d675502c982fc7bc7d9094d17
Sean McPherson <git@seanmcp.com>
2019-08-19 22:36:05 -0400

    Use form, submit to add class

--- src/js/home.js
+++ src/js/home.js
@@ -1,39 +1,42 @@
-var classList = document.getElementById('class-list');
-var classCount = document.getElementById('class-count');
-var classNameInput = document.getElementById('name-input');
-var addClassButton = document.getElementById('add-button');
+var classList = document.getElementById('class-list')
+var classCount = document.getElementById('class-count')
+var classNameInput = document.getElementById('name-input')
 
 chrome.storage.sync.get('classes', function(result) {
-    var count = 0;
+    var count = 0
     if (Object.keys(result.classes).length) {
         for (var id in result.classes) {
-            var li = document.createElement('li');
-            var a = document.createElement('a');
-            a.href = `class.html#${id}`;
-            a.textContent = result.classes[id];
-            li.appendChild(a);
-            classList.appendChild(li);
-        };
-        count = Object.keys(result.classes).length;
+            var li = document.createElement('li')
+            var a = document.createElement('a')
+            a.href = `class.html#${id}`
+            a.textContent = result.classes[id]
+            li.appendChild(a)
+            classList.appendChild(li)
+        }
+        count = Object.keys(result.classes).length
     } else {
-        classList.appendChild(buildNoneFoundElement('li'));
+        classList.appendChild(buildNoneFoundElement('li'))
     }
-    classCount.textContent = `Classes (${count})`;
-});
+    classCount.textContent = `Classes (${count})`
+})
 
-function addClass() {
+function addClass(event) {
+    event.preventDefault()
     chrome.storage.sync.get('classes', function(result) {
-        var classes = cloneObj(result.classes);
-        var id = new Date().getTime();
-        classes[id] = classNameInput.value;
+        var classes = cloneObj(result.classes)
+        var id = new Date().getTime()
+        classes[id] = classNameInput.value
 
-        chrome.storage.sync.set({
-            classes: classes,
-        }, function() {
-            classNameInput.value = '';
-            document.location = `class.html#${id}`
-        });
-    });
+        chrome.storage.sync.set(
+            {
+                classes: classes
+            },
+            function() {
+                classNameInput.value = ''
+                document.location = `class.html#${id}`
+            }
+        )
+    })
 }
 
-addClassButton.addEventListener('click', addClass);
+document.forms['add-class'].onsubmit = addClass
