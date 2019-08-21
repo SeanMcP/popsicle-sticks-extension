a5b99a820dc7f195caeb9c5b56a1d1eaa5b5b311
Sean McPherson <git@seanmcp.com>
2019-08-21 00:11:27 -0400

    Add upload

--- src/js/backup.js
+++ src/js/backup.js
@@ -20,3 +20,45 @@ function downloadFile() {
 }
 
 document.forms['download-data'].onsubmit = downloadFile
+
+function saveUploadedData(event) {
+    const result = JSON.parse(event.target.result)
+    if (result.secret === secret) {
+        const dataToSet = {}
+        if (result.hasOwnProperty('classes')) dataToSet.classes = result.classes
+        if (result.hasOwnProperty('studentsByClassId'))
+            dataToSet.studentsByClassId = result.studentsByClassId
+
+        chrome.storage.sync.set(dataToSet, function() {
+            document.location = 'home.html'
+        })
+    } else {
+        handleUploadError()
+    }
+}
+
+const uploadForm = document.forms['upload-data']
+
+function uploadFile(event) {
+    event.preventDefault()
+    const file = new FormData(event.target).get('file')
+
+    const reader = new FileReader()
+    reader.onload = saveUploadedData
+    reader.readAsBinaryString(file)
+}
+
+function handleUploadError() {
+    const message = document.createElement('p')
+    message.classList.add('error-message')
+    message.textContent = "Uh oh! That data wasn't formatted correctly."
+    uploadForm.reset()
+    uploadForm.classList.add('--error')
+    uploadForm.appendChild(message)
+    setTimeout(() => {
+        uploadForm.classList.remove('--error')
+        uploadForm.removeChild(message)
+    }, 5000)
+}
+
+uploadForm.onsubmit = uploadFile
