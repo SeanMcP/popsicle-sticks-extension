03be47fea2b22875363194cc9b87ac6a279a5e7e
Sean McPherson <git@seanmcp.com>
2019-08-29 20:54:19 -0400

    Update build script

--- build.sh
+++ build.sh
@@ -4,16 +4,4 @@ echo "Version (e.g. 1.1)"
 
 read version
 
-mkdir temp/ build/
-
-rm -rf build/*
-
-cp -r src/ temp/ && cp manifest.json temp/
-
-cd temp/
-
-zip -r -X ../build/popsicle-sticks-extension-v$version.zip .
-
-cd ..
-
-rm -rf temp/
+zip -r build-v$version.zip ./ -x "*.DS_Store" "*.svg" \*.git\* "*README.md" \*.chrome\*
