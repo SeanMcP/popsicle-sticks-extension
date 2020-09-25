bb32ca9504dc36a0af90a4de219ebe2fb6dff202
Sean McPherson <smcpherson@niche.com>
2020-09-24 21:00:18 -0400

    feat: bulk add poc

---
+++ src/js/bulk.js
@@ -0,0 +1,53 @@
+// @ts-check
+
+const classId = getId();
+
+const classLink = document.getElementById("class-link");
+classLink.href = classLink.href + "?id=" + classId;
+
+const bulkAddForm = document.getElementById("bulk-add");
+
+bulkAddForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const fd = new FormData(event.target);
+  const students = fd.get("students");
+  const delineator = fd.get("delineator");
+
+  let names = []
+
+  switch (delineator) {
+    case 'newline': {
+      names = students.split("\r\n")
+      break;
+    }
+    case 'comma': {
+      names = students.split(",")
+      break;
+    }
+  }
+
+  if (names.length === 0) return;
+
+  chrome.storage.sync.get("studentsByClassId", (result) => {
+    const studentsByClassId = cloneObj(result.studentsByClassId);
+
+    if (!studentsByClassId[classId]) {
+      studentsByClassId[classId] = {};
+    }
+
+    names.forEach((name) => {
+      const id = new Date().getTime() * Math.random();
+      studentsByClassId[classId][id] = name;
+    });
+
+    chrome.storage.sync.set(
+      {
+        studentsByClassId,
+      },
+      () => {
+        document.location = `class.html?id=${classId}`;
+      }
+    );
+  });
+});
