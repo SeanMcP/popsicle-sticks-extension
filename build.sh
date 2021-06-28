8fd5eb17b9694c3f04d27b94b13fac2f777918cd
Sean McPherson <sean.mcpherson@niche.com>
2021-06-28 16:55:21 -0400

    feat: add version to settings view
    Closes #24

--- build.sh
+++ build.sh
@@ -2,4 +2,12 @@
 
 version=$(cat ./manifest.json | node -pe 'JSON.parse(fs.readFileSync(0)).version')
 
-zip -r build-v$version.zip ./manifest.json ./src/ -x "*.svg"
+to_replace="%VERSION_NUMBER%"
+
+sed -i '' "s/$to_replace/$version/g" ./src/views/settings.html
+
+# zip -r build-v$version.zip ./manifest.json ./src/ -x "*.svg"
+
+# There is probably a better way to do this.
+sed -i '' "s/$version/$to_replace/g" ./src/views/settings.html
+
