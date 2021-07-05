0c945992bd67d872043f6c4ce9d507b45c267393
Sean McPherson <sean.mcpherson@niche.com>
2021-07-05 06:55:47 -0400

    feat: add addToHistory fn

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,35 @@
-console.log('Up and running 🏃‍♂️')
+console.log("Up and running 🏃‍♂️");
+
+function addToHistory(data = null) {
+  chrome.storage.sync.set({
+    history: {
+      data,
+      path: window.location.pathname + window.location.search,
+      timestamp: new Date().getTime(),
+    },
+  });
+}
 
 function cloneObj(obj) {
-    return JSON.parse(JSON.stringify(obj))
+  return JSON.parse(JSON.stringify(obj));
 }
 
 function buildNoneFoundElement(tag) {
-    var el = document.createElement(tag)
-    el.classList.add('none-found')
-    el.textContent = 'None found'
-    return el
+  var el = document.createElement(tag);
+  el.classList.add("none-found");
+  el.textContent = "None found";
+  return el;
 }
 
 function getId() {
-    const params = new URLSearchParams(window.location.search)
-    return params.get('id') || ''
+  const params = new URLSearchParams(window.location.search);
+  return params.get("id") || "";
 }
 
 function setTheme() {
-    chrome.storage.sync.get('theme', result => {
-        document.body.dataset.theme = result.theme
-    })
+  chrome.storage.sync.get("theme", (result) => {
+    document.body.dataset.theme = result.theme;
+  });
 }
 
-setTheme()
+setTheme();
