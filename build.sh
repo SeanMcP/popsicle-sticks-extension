0d98841782b56bf6a998b2ef3ca1a56356e0a880
Sean McPherson <sean.mcpherson@niche.com>
2021-08-14 13:16:11 -0400

    fix: uncomment build line

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ to_replace="%VERSION_NUMBER%"
 
 sed -i '' "s/$to_replace/$version/g" ./src/views/settings.html
 
-# zip -r build-v$version.zip ./manifest.json ./src/ -x "*.svg"
+zip -r build-v$version.zip ./manifest.json ./src/ -x "*.svg"
 
 # There is probably a better way to do this.
 sed -i '' "s/$version/$to_replace/g" ./src/views/settings.html
