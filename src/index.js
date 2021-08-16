70be59299329bfcfec55dc8703cf5edacce37340
Sean McPherson <sean.mcpherson@niche.com>
2021-08-16 13:32:27 -0400

    task: migrate to new data structure

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.sync.get(["initiated", "history"], (result) => {
-    if (result.initiated) {
+  chrome.storage.sync.get(["classes", "v", "studentsByClassId"], (result) => {
+    if (!result.v || result.v != 1) {
+      const classIds = Object.keys(result.classes);
+      if (classIds && classIds.length) {
+        // Migrate to new format
+        const classes = {};
+        const studentsByClassId = {};
+        classIds.forEach((id, i) => {
+          const newId = "c" + i;
+          classes[newId] = result.classes[id];
+          if (result.studentsByClassId[id]) {
+            studentsByClassId[newId] = Object.values(
+              result.studentsByClassId[id]
+            );
+          }
+        });
+
+        chrome.storage.sync.set(
+          {
+            classes,
+            studentsByClassId,
+            v: 1,
+          },
+          () => {
+            chrome.storage.sync.remove(["history", "initiated"]);
+          }
+        );
+      }
+
       return;
-    }
-    if (typeof result.history !== undefined) {
-      chrome.storage.sync.clear("history");
     }
 
     chrome.storage.sync.set({
       classes: {},
-      initiated: true,
       studentsByClassId: {},
       theme: "light",
+      v: 1,
     });
   });
 });
--- src/js/class.js
+++ src/js/class.js
@@ -20,17 +20,16 @@ function addStudent(event) {
   event.preventDefault();
   if (studentNameInput.value) {
     chrome.storage.sync.get("studentsByClassId", function (result) {
-      let studentsByClassId = cloneObj(result.studentsByClassId);
+      const studentsByClassId = cloneObj(result.studentsByClassId);
 
-      let studentId = new Date().getTime();
       if (!studentsByClassId[classId]) {
-        studentsByClassId[classId] = {};
+        studentsByClassId[classId] = [];
       }
-      studentsByClassId[classId][studentId] = studentNameInput.value;
+      studentsByClassId[classId].push(studentNameInput.value);
 
       chrome.storage.sync.set(
         {
-          studentsByClassId: studentsByClassId,
+          studentsByClassId,
         },
         function () {
           studentNameInput.value = "";
@@ -41,10 +40,12 @@ function addStudent(event) {
   }
 }
 
-function deleteStudent(id) {
+function deleteStudent(name) {
   chrome.storage.sync.get(["studentsByClassId"], function (result) {
-    let studentsByClassId = cloneObj(result.studentsByClassId);
-    delete studentsByClassId[classId][id];
+    const studentsByClassId = cloneObj(result.studentsByClassId);
+    studentsByClassId[classId] = studentsByClassId[classId].filter(
+      (item) => item !== name
+    );
 
     chrome.storage.sync.set(
       {
@@ -84,15 +85,15 @@ function renderStudents() {
   chrome.storage.sync.get(["classes", "studentsByClassId"], function (result) {
     let students = result.studentsByClassId[classId];
     let count = 0;
-    if (students && Object.keys(students).length) {
-      const ordered = Object.entries(students);
+    if (students && students.length) {
+      const ordered = [...students];
 
       // TODO: Consider making this a setting
       ordered.sort((a, b) => {
-        return getBestValue(a[1]) < getBestValue(b[1]) ? -1 : 1;
+        return getBestValue(a) < getBestValue(b) ? -1 : 1;
       });
 
-      ordered.forEach(([id, name]) => {
+      ordered.forEach((name) => {
         let li = document.createElement("li");
         let span = document.createElement("span");
         span.textContent = name;
@@ -106,7 +107,7 @@ function renderStudents() {
         button.setAttribute("title", "Remove student");
 
         button.addEventListener("click", function () {
-          deleteStudent(id);
+          deleteStudent(name);
         });
         li.appendChild(button);
 
